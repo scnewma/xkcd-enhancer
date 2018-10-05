b0ec46ab866b25f8d4526601c521c78178fe8d5e
Shaun Newman <scnewmadev@live.com>
2018-10-04 20:56:44 -0700

    binds p and n shortcuts

--- enhance.js
+++ enhance.js
@@ -47,5 +47,38 @@
         }
     }
 
+    function handleKeyPress(e) {
+        if (isModified(e)) {
+            return;
+        }
+
+        let key = e.key;
+
+        if (key === 'p') {
+            navigateToPreviousPage();
+        } else if (key === 'n') {
+            navigateToNextPage();
+        }
+    }
+
+    function isModified(e) {
+        return e.metaKey || e.altKey || e.ctrlKey;
+    }
+
+    function navigateToPreviousPage() {
+        let prevAnchor = retrieveNavLinkByRel('prev');
+        location.assign(prevAnchor.href);
+    }
+
+    function navigateToNextPage() {
+        let nextAnchor = retrieveNavLinkByRel('next');
+        location.assign(nextAnchor.href);
+    }
+
+    function retrieveNavLinkByRel(rel) {
+        return document.querySelector(`.comicNav a[rel=${rel}]`);
+    }
+
     displayComicSubtext();
+    window.addEventListener('keypress', handleKeyPress);
 })();
